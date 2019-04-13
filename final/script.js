5f552f3dd036af5cdfa7c11e6e28bc4363dda716
Kaasa-Alec <kaa15001@byui.edu>
2019-04-13 14:19:25 -0700

    Update script.js

--- final/script.js
+++ final/script.js
@@ -1,303 +1,207 @@
-    // Timezone for clock IF statement.  If empty, displays time as normal.  If not, displays time in time zone.
-    var zone = "";
+// Timezone for clock IF statement.  If empty, displays time as normal.  If not, displays time in time zone.
+var zone = "";
     
-    // Print minute/hour lines
-    function timeLines() {
+// Print minute/hour lines
+function timeLines() {
       
-      var lines = document.getElementsByClassName('lines');
-      var clockEdge = document.getElementsByClassName('clock')[0];
+  var lines = document.getElementsByClassName('lines'),
+  clockEdge = document.getElementsByClassName('clock')[0];
 
-      for (var i = 1; i < 60; i++) {
-        clockEdge.innerHTML += "<div class='lines'></div>";
-        lines[i].style.transform = "rotate(" + 6 * i + "deg)";
-      }
+  for (var i = 1; i < 60; i++) {
+    clockEdge.innerHTML += "<div class='lines'></div>";
+    lines[i].style.transform = "rotate(" + 6 * i + "deg)";
+  }
 
+}
+
+// Analog Clock
+function clock(){
+
+  // Local time.  Changes for timezone if not empty.
+  if (zone == "") {
+
+    // Time element grabbers
+    var date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    session = "AM";
+
+    // Hand angles
+    var hAngle = h * 30 + m * (360/720),
+    mAngle = m * 6 + s * (360/3600),
+    sAngle = s * 6;
+
+    // Hand grabbers
+    var hHand = document.querySelector('.hour'),
+    mHand = document.querySelector('.minute'),
+    sHand = document.querySelector('.second');
+
+    // Angle rotation
+    hHand.style.transform = "rotate(" + hAngle + "deg)";
+    mHand.style.transform = "rotate(" + mAngle + "deg)";
+    sHand.style.transform = "rotate(" + sAngle + "deg)";
+
+    // Digital Clock
+    // Session Setter
+    if (h >= 12) {
+      session = "PM";
     }
 
-    // Analog Clock
-    function clock(){
-
-      // Default computer clock for local time, else: alternate timezone clock
-      if (zone == "") {
-
-        // Time element grabbers
-        var date = new Date();
-        var hour = date.getHours();
-        var minute = date.getMinutes();
-        var second = date.getSeconds();
-        var session = "AM";
-
-        // Hand angles
-        var hourAngle = hour * 30 + minute * (360/720);
-        var minuteAngle = minute * 6 + second * (360/3600);
-        var secondAngle = second * 6;
-
-        // Hand grabbers
-        var hourHand = document.querySelector('.hour');
-        var minuteHand = document.querySelector('.minute');
-        var secondHand = document.querySelector('.second');
-
-        // Angle rotation
-        hourHand.style.transform = "rotate(" + hourAngle + "deg)";
-        minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
-        secondHand.style.transform = "rotate(" + secondAngle + "deg)";
-
-      // Digital Clock
-        // Session Setter
-        if (hour >= 12) {
-          session = "PM";
-        }
-
-        // Standard Time Setter (instead of Military Time)
-        if (hour == 0) {
-          hour = 12;
-          } else if (hour > 12) {
-            hour -= 12;
-        }
-
-        if (minute < 10) {
-          minute = "0" + minute;
-        }
-
-        if (second < 10) {
-          second = "0" + second;
-        }
-
-        // Assemble and Display Digital Time
-        var time = hour + ":" + minute + ":" + second + " " + session;
-        document.getElementById("digital").innerText = time;
-        document.getElementById("digital").textContent = time;
-
-      } else {
-
-        switch(zone) {
-          case "local":
-
-            break;
-          case "eastern":
-
-            var timeZone = new TimeZoneDB;
-            var offset = 10;
-
-            timeZone.getJSON({
-              key: "FQASQV7RU9MQ",
-              lat: 34.0536834,
-              lng: -118.2427669
-            }, function (timeZone) {
-          
-              newClock(timeZone.timestamp, offset);
-
-            });
-
-            break;
-          case "central":
-
-            var timeZone = new TimeZoneDB;
-            var offset = 9;
-
-            timeZone.getJSON({
-              key: "FQASQV7RU9MQ",
-              lat: 34.0536834,
-              lng: -118.2427669
-            }, function (timeZone) {
-          
-              newClock(timeZone.timestamp, offset);
-
-            });
-              
-            break;
-          case "mt":
-
-            var timeZone = new TimeZoneDB;
-            var offset = 8;
-
-            timeZone.getJSON({
-              key: "FQASQV7RU9MQ",
-              lat: 33.4484367,
-              lng: -112.0741417
-            }, function (timeZone) {
-          
-              newClock(timeZone.timestamp, offset);
-
-            });
-              
-            break;
-          case "pdt":
-
-            var timeZone = new TimeZoneDB;
-            var offset = 7;
-
-            timeZone.getJSON({
-              key: "FQASQV7RU9MQ",
-              lat: 34.0536834,
-              lng: -118.2427669
-            }, function (timeZone) {
-          
-              newClock(timeZone.timestamp, offset);
-
-            });
-              
-            break;
-          case "adt":
-
-            var timeZone = new TimeZoneDB;
-            var offset = 7;
-
-            timeZone.getJSON({
-              key: "FQASQV7RU9MQ",
-              lat: 61.2163129,
-              lng: -149.8948523
-            }, function (timeZone) {
-          
-              newClock(timeZone.timestamp, offset);
-
-            });
-              
-            break;
-          case "hast":
-
-            var timeZone = new TimeZoneDB;
-            var offset = 7;
-
-            timeZone.getJSON({
-              key: "FQASQV7RU9MQ",
-              lat: 21.304547,
-              lng: -157.8556764
-            }, function (timeZone) {
-          
-              newClock(timeZone.timestamp, offset);
-
-            });
-              
-            break;
-        }
-      }
+    // Standard Time Setter (instead of Military Time)
+    if (h == 0) {
+      h = 12;
+      } else if (h > 12) {
+        h -= 12;
     }
 
-    function changeTimeZone(timeZone) {
-
-      var timeZone = document.getElementById("timeZones").value;
-
-      switch(timeZone) {
-        case "local":
-          zone = "";
-          
-          break;
-        case "eastern":
-          zone = "eastern";
-          
-          break;
-        case "central":
-          zone = "central";
-          
-          break;
-        case "mt":
-          zone = "mt";
-          
-          break;
-        case "pdt":
-          zone = "pdt";
-          
-          break;
-        case "adt":
-          zone = "adt";
-          
-          break;
-        case "hast":
-          zone = "hast";
-          
-          break;
-        default:
-          zone = "";
-          document.getElementById("timeZones").innerHTML = "Local Time";
-        }
+    // If minute/second is single digits, precede with 0
+    if (m < 10) {
+      m = "0" + m;
     }
 
-    // New Clock for a different time zone
-    function newClock(timestamp, offset) {
-
-      var time = new Date(timestamp*1000);
-      // Hours part from the timestamp
-      var hour = time.getHours() + offset;
-
-      if (hour >= 24) {
-        hour -= 24;
-      }
-
-      var x = ((hour + 11) % 12) + 1;
-
-      var session = hour > 11 ? "PM" : "AM";
-
-      var minute = "0" + time.getMinutes();
-      var second = "0" + time.getSeconds();
-
-      // Display Digital Time
-        var digitalTime = x + ':' + minute.substr(-2) + ':' + second.substr(-2) + " " + session;
-
-        document.getElementById("digital").innerText = digitalTime;
-        document.getElementById("digital").textContent = digitalTime;
-
-      // Display Analog Time
-        // Hand angles
-        var hourAngle = x * 30 + minute * (360/720);
-        var minuteAngle = minute * 6 + second * (360/3600);
-        var secondAngle = second * 6;
-
-        // Hand grabbers
-        var hourHand = document.querySelector('.hour');
-        var minuteHand = document.querySelector('.minute');
-        var secondHand = document.querySelector('.second');
-
-        // Angle rotation
-        hourHand.style.transform = "rotate(" + hourAngle + "deg)";
-        minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
-        secondHand.style.transform = "rotate(" + secondAngle + "deg)";
-
+    if (s < 10) {
+      s = "0" + s;
     }
 
-    // Pull the data from the TimeZone database. Function is the same one from the link before.
-    function TimeZoneDB() {
-      this.getJSON=function(e,t) {
-        try {
-          var n = e.key,
-          o = e.zone,
-          a = e.lat,
-          i = e.lng,
-          c = e.time,
-          r = "_" + (new Date).getTime() + Math.floor(1e4 * Math.random()),
-          d = document.createElement("script");
-          window[r] = function(e) {
+    // Assemble and Display Digital Time
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("digital").innerText = time;
+    document.getElementById("digital").textContent = time;
+
+  } else {
+
+    switch(zone) {
+      case "local":
+
+        break;
+      case "eastern":
+        var timeZone = "America/New_York";
+        getTimeZone(timeZone);
+        break;
+
+      case "central":
+        var timeZone = "America/Chicago";
+        getTimeZone(timeZone);
+        break;
+
+      case "mt":
+        var timeZone = "America/Denver";
+        getTimeZone(timeZone);
+        break;
+
+      case "pdt":
+        var timeZone = "America/Los_Angeles";
+        getTimeZone(timeZone);
+        break;
             
-            t&&t(e);
+      case "adt":
+        var timeZone = "America/Anchorage";
+        getTimeZone(timeZone);
+        break;
 
-            try {
-              delete window[r]
-            } catch(n) {
-              window[r]=void 0
-              }
-          },
-          
-          o?d.src = "//api.timezonedb.com/?key=" 
-          + n 
-          + "&zone=" 
-          + o 
-          + (c?"&time=" + c:"") 
-          + "&format=json&callback=" 
-          + r:a&&i&&(d.src="//api.timezonedb.com/?key=" 
-          + n 
-          + (c?"&time="+c:"") 
-          + "&lat="
-          + a 
-          + "&lng="
-          + i 
-          + "&format=json&callback="
-          +r),document.body.appendChild(d),d.parentNode.removeChild(d)
-            
-        } catch(m) {
-          return alert("JSON request failed."),!1
-        }
-      }
+      case "hast":
+        var timeZone = "Pacific/Honolulu";
+        getTimeZone(timeZone);
+        break;
+
     }
+  }
+}
 
-    timeLines();
-    setInterval(clock, 1000);
+function changeTimeZone(timeZone) {
+
+  var timeZone = document.getElementById("timeZones").value;
+
+  switch(timeZone) {
+    case "local":
+      zone = "";
+      break;
+    case "eastern":
+      zone = "eastern";  
+      break;
+    case "central":
+      zone = "central";  
+      break;
+    case "mt":
+      zone = "mt";  
+      break;
+    case "pdt":
+      zone = "pdt";  
+      break;
+    case "adt":
+      zone = "adt";  
+      break;
+    case "hast":
+      zone = "hast";  
+      break;
+    default:
+      zone = "";
+      document.getElementById("timeZones").innerHTML = "Local Time";
+  }
+}
+
+// New Clock for a different time zone
+function newClock(timestamp, offset) {
+
+  // Format time
+  var time = new Date(timestamp*1000);
+  // Hours part from the timestamp
+  var h = time.getHours() + offset;
+
+  if (h >= 24) {
+    h -= 24;
+  }
+
+  var hFixed = ((h + 11) % 12) + 1,
+  session = h > 11 ? "PM" : "AM",
+  m = "0" + time.getMinutes(),
+  s = "0" + time.getSeconds();
+
+  // Display Digital Time
+  var digitalTime = hFixed + ':' + m.substr(-2) + ':' + s.substr(-2) + " " + session;
+
+  document.getElementById("digital").innerText = digitalTime;
+  document.getElementById("digital").textContent = digitalTime;
+
+  // Display Analog Time
+  // Hand angles
+  var hAngle = hFixed * 30 + m * (360/720);
+  var mAngle = m * 6 + s * (360/3600);
+  var sAngle = s * 6;
+
+  // Hand grabbers
+  var hHand = document.querySelector('.hour');
+  var mHand = document.querySelector('.minute');
+  var sHand = document.querySelector('.second');
+
+  // Angle rotation
+  hHand.style.transform = "rotate(" + hAngle + "deg)";
+  mHand.style.transform = "rotate(" + mAngle + "deg)";
+  sHand.style.transform = "rotate(" + sAngle + "deg)";
+
+}
+
+// Retrieves timestamp and calls New Clock with it
+function getTimeZone(zone) {
+
+  var offset = 7;
+
+  var req = new XMLHttpRequest();
+
+  req.open('GET', 'https://api.timezonedb.com/v2.1/get-time-zone?key=FQASQV7RU9MQ&format=json&by=zone&zone=' + zone, true);
+            
+  req.onload = function () {
+
+    var data = JSON.parse(req.responseText)
+
+    var time = data.timestamp;
+
+    newClock(time, offset);
+              
+  };
+  req.send(null);
+}
+
+timeLines();
+setInterval(clock, 1000);
